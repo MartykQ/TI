39e33c53c32ece60e009f22012acd52fdedcd6ad
MartykQ <fmartyka16@gmail.com>
2019-02-27 11:57:16 +0100

    added

---
+++ app/js/script1.js
@@ -0,0 +1,34 @@
+/* function wiek(rokUrodzenia) {
+    let wynik = (new Date()).getFullYear() - rokUrodzenia;
+    return wynik;
+}
+
+function oblicz(){
+    let rok = document.getElementById("wiek").value;
+    alert(wiek(rok));
+} */
+/* 
+let oceny = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,6];
+let oceny1 = [];
+oceny1.push(1);
+alert(srednia(oceny));
+
+function srednia(tablica) {
+/*     let suma = 0;
+    for(let i = 0; i <oceny.length; i++) {
+        suma += oceny[i];
+    }
+    return suma/oceny.length; */
+/* 
+    let wynik=0;
+    tablica.forEach(element => {
+        wynik += element;
+    });
+    return wynik/tablica.length; //to samo tylko z reduce
+}
+
+function wypisz(){
+    document.write("<ul>");
+    oceny.forEach((x)=> {
+        document.write("<li>"+x+"</li>");});
+} */ 
